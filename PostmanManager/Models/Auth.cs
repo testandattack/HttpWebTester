2c91357852dea3c637d219371d56049853a71e41
Gray, Geoffrey L UTAS <GRAYGEOL@UTCAUS.com>
2022-06-30 14:39:41 -0400

    syncing with server

--- PostmanManager/Models/Auth.cs
+++ PostmanManager/Models/Auth.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +8,84 @@ namespace PostmanManager.Models
 {
     public class Auth
     {
-        public Auth()
-        {
-            throw new NotImplementedException;
-        }
+        [JsonProperty("type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public AuthType_Enum Type { get; set; }
+        
+        [JsonProperty("noauth")]
+        public object NoAuth { get; set; }
+
+        /// <summary>
+        /// The attributes for API Key Authentication.
+        /// </summary>
+        [JsonProperty("apikey")]
+        public List<AuthAttribute> ApiKey { get; set; }
+
+        /// <summary>
+        /// The attributes for [AWS Auth](http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html).
+        /// </summary>
+        [JsonProperty("awsv4")]
+        public List<AuthAttribute> AwsV4 { get; set; }
+
+        /// <summary>
+        /// The attributes for [Basic Authentication](https://en.wikipedia.org/wiki/Basic_access_authentication).
+        /// </summary>
+        [JsonProperty("basic")]
+        public List<AuthAttribute> Basic { get; set; }
+
+        /// <summary>
+        /// The helper attributes for [Bearer Token Authentication](https://tools.ietf.org/html/rfc6750)
+        /// </summary>
+        [JsonProperty("bearer")]
+        public List<AuthAttribute> Bearer { get; set; }
+
+        /// <summary>
+        /// The attributes for [Digest Authentication](https://en.wikipedia.org/wiki/Digest_access_authentication).
+        /// </summary>
+        [JsonProperty("digest")]
+        public List<AuthAttribute> Digest { get; set; }
+
+        /// <summary>
+        /// The attributes for [Akamai EdgeGrid Authentication](https://developer.akamai.com/legacy/introduction/Client_Auth.html).
+        /// </summary>
+        [JsonProperty("edgegrid")]
+        public List<AuthAttribute> EdgeGrid { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("")]
+        public List<AuthAttribute> Hawk { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("")]
+        public List<AuthAttribute> Ntlm { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("")]
+        public List<AuthAttribute> OAuth1 { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty("")]
+        public List<AuthAttribute> OAuth2 { get; set; }
+    }
+
+    public class AuthAttribute
+    {
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("value")]
+        public object Value { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
     }
 }
--- PostmanManager/Models/Version.cs
+++ PostmanManager/Models/Version.cs
@@ -1,14 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace PostmanManager.Models
 {
+    /// <summary>
+    /// Postman allows you to version your collections as they grow, 
+    /// and this field holds the version number. While optional, 
+    /// it is recommended that you use this field to its fullest extent!
+    /// </summary>
     public class Version
     {
-        public Version()
-        {
-            throw new NotImplementedException;
-        }
+        /// <summary>
+        /// Increment this number if you make changes to the collection that changes its behaviour. 
+        /// E.g: Removing or adding new test scripts. (partly or completely).
+        /// </summary>
+        [JsonProperty("major")]
+        public int Major { get; set; }
+
+        /// <summary>
+        /// You should increment this number if you make changes that will not break 
+        /// anything that uses the collection. E.g: removing a folder.
+        /// </summary>
+        [JsonProperty("minor")]
+        public int Minor { get; set; }
+
+        /// <summary>
+        /// Ideally, minor changes to a collection should result in the increment of this number.
+        /// </summary>
+        [JsonProperty("patch")]
+        public int Patch { get; set; }
+
+        /// <summary>
+        /// A human friendly identifier to make sense of the version numbers. E.g: 'beta-3'
+        /// </summary>
+        [JsonProperty("major")]
+        public string Identifier { get; set; }
     }
 }
